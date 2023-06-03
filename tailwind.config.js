bf120cb6dea908aa4efbde8d5a2384d07d98c216
jisooyu <jisooyu@hotmail.com>
2023-06-03 21:01:18 +0900

    fixing import error

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,17 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./build/*.html",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter:['Inter', 'sans-serif']
+      },
+      colors:{
+        'primary-orange':'#FF5722',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
